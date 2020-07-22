21718a6624f471ff582478705144e64bd05e5de0
jun fujimura <j-fujimura@imjp.co.jp>
2020-07-22 19:03:53 +0900

    いひ

--- resources/src/App.vue
+++ resources/src/App.vue
@@ -3,18 +3,21 @@
     <p>hogehgoe</p>
     <img alt="Vue logo" src="/img/test.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SandboxStage1></SandboxStage1>
   </div>
 </template>
 
 <script lang="ts">
 import HelloWorld from '~/components/HelloWorld.vue';
 import ImagePreview from '~/components/ImagePreview.vue';
+import SandboxStage1 from '~/components/SandboxStage1.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
     ImagePreview,
+    SandboxStage1,
   },
 };
 </script>
---
+++ resources/src/components/SandboxStage1.vue
@@ -0,0 +1,49 @@
+<template>
+  <dl class="stage">
+    <dt class="stage-title">count</dt>
+    <dd class="stage-detail">{{ state.count }}</dd>
+  </dl>
+  <button @click="increment">+1</button>
+  <button @click="decrement">-1</button>
+</template>
+
+<script lang="ts">
+import { reactive, computed } from 'vue';
+
+export default {
+  setup() {
+    const state = reactive<{ count: number }>({
+      count: 0,
+    });
+
+    function increment() {
+      state.count++;
+    }
+
+    function decrement() {
+      state.count--;
+    }
+
+    return {
+      state,
+      increment,
+      decrement,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.stage {
+  display: flex;
+  justify-content: center;
+
+  &-title {
+    font-weight: bold;
+  }
+
+  &-detail {
+    color: #333;
+  }
+}
+</style>
