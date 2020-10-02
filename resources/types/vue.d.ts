04634363d07423e91550501e08b1e821503f80a0
svjunic <sv.junic@gmail.com>
2020-10-02 10:01:10 +0900

    ts動かなくなったので対応

---
+++ resources/types/vue.d.ts
@@ -0,0 +1,11 @@
+//こっちじゃないらしい
+//declare module '*.vue' {
+//  import Vue from 'vue';
+//  export default Vue;
+//}
+
+declare module '*.vue' {
+  import { defineComponent } from 'vue';
+  const component: ReturnType<typeof defineComponent>;
+  export default component;
+}
